9c1b650bbd1b315cd501976ee0b8afcb4b763719
RAFAELA DOS SANTOS PROENCA <0000110652388xsp@aluno.educacao.sp.gov.br>
2024-05-28 11:29:24 -0300

    criando função somar

---
+++ script.js
@@ -0,0 +1,7 @@
+function somarNumeros(){
+   
+    var numero1 = parseInt(document.getElementById("num1").value);
+    var numero2 = parseInt(document.getElementById("num2").value);
+    var resultado = numero1 + numero2
+    alert (resultado)
+}
